4851cf2a5c656e484a953722433281cf6aeb0c9b
Lasse Haverinen <lasse.haverinen@gmail.com>
2017-10-06 17:56:23 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+});
+
+
+app.get('/test', function (req, res) {
+    res.send({
+        id: 1,
+        description: "Hello World"
+    });
+});
+
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
